c48e47a3dbc12befeac30465fd506e48d3bab525
dammi <36568916+dammi-i@users.noreply.github.com>
2021-03-05 19:50:31 +0100

    we care

--- compile_time/go_to_bed.cpp
+++ compile_time/go_to_bed.cpp
@@ -1,5 +1,5 @@
 #include <string_view>
-
+// we care about our customers remember my words: "sleep is the key to strong thought" 
 
 
 static_assert("23:00:00" >= std::string_view(__TIME__) && std::string_view(__TIME__) >= "06:00:00", "go to bed");
